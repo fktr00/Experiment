d8840efa246116cc754ebf4345d4994ba90b1506
Aleksei <fktr00@mail.ru>
2022-01-09 21:06:08 +0200

    _

--- Py/hhj.py
+++ Py/hhj.py
@@ -1,1 +1,7 @@
-print('hg')
+import pandas as pd
+import os
+current_directory = os.getcwd()
+print(current_directory) 
+
+df = pd.read_csv("/home/fktr0/Documents/GitHub/fktr00/experiment/JP/data/data.csv")
+print(df)
---
+++ Py/untitled4.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  3 14:20:48 2022
+
+@author: fktr0
+"""
+
