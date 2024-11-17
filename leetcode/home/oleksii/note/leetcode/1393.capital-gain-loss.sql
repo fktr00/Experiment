37fe2418dfd69a265d8fa1b7dbba852b75497e91
Oleksii <fktr00@gmail.com>
2024-11-17 16:00:11 +0200

    1393 LeetCode

--- leetcode/home/oleksii/note/leetcode/1393.capital-gain-loss.sql
+++ leetcode/home/oleksii/note/leetcode/1393.capital-gain-loss.sql
@@ -6,6 +6,14 @@
 
 -- @lc code=start
 -- Write your PostgreSQL query statement below
-
+select 
+    stock_name,
+    sum(case 
+        when s.operation='Buy' then -price 
+        else price
+    
+    end) as capital_gain_loss
+from stocks as s
+group by s.stock_name
 -- @lc code=end
 
